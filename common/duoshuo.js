a74b526df6218b3eba493882f9263009fa3b2d0e
asher <1057488870@qq.com>
2016-10-19 04:04:30 +0800

    fc

--- common/duoshuo.js
+++ common/duoshuo.js
@@ -1,21 +1,21 @@
 var duoshuoQuery, duoshuoQueryRun;
 
 duoshuoQuery = {
-  short_name: "writeyoursmile"
+    short_name: "writeyoursmile"
 };
 
-duoshuoQueryRun = function() {
-  var ds, protocol;
-  ds = document.createElement('script');
-  ds.type = 'text/javascript';
-  ds.async = true;
-  protocol = 'http:';
-  if (document.location.protocol === 'https:') {
-    protocol = 'https:';
-  }
-  ds.src = protocol + '//static.duoshuo.com/embed.js';
-  ds.charset = 'UTF-8';
-  return (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
+duoshuoQueryRun = function () {
+    var ds, protocol;
+    ds = document.createElement('script');
+    ds.type = 'text/javascript';
+    ds.async = true;
+    protocol = 'http:';
+    if (document.location.protocol === 'https:') {
+        protocol = 'https:';
+    }
+    ds.src = protocol + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    return (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
 };
 
 duoshuoQueryRun();
