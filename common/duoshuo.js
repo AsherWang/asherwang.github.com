e5ee71d864aa8fb0ea2e7fd8633a99f97288a413
asher <1057488870@qq.com>
2016-10-19 03:58:06 +0800

    fc

--- common/duoshuo.js
+++ common/duoshuo.js
@@ -1,1 +1,19 @@
-var duoshuoQuery,duoshuoQueryRun;duoshuoQuery={short_name:"writeyoursmile"},(duoshuoQueryRun=function(){var e,t;return e=document.createElement("script"),e.type="text/javascript",e.async=!0,e.src=(null!=(t="https:"===document.location.protocol)?t:{"https:":"http:"})+"//static.duoshuo.com/embed.js",e.charset="UTF-8",(document.getElementsByTagName("head")[0]||document.getElementsByTagName("body")[0]).appendChild(e)})();
+var duoshuoQuery, duoshuoQueryRun;
+
+duoshuoQuery = {
+  short_name: "writeyoursmile"
+};
+
+duoshuoQueryRun = function() {
+  var ds, ref;
+  ds = document.createElement('script');
+  ds.type = 'text/javascript';
+  ds.async = true;
+  ds.src = ((ref = document.location.protocol === 'https:') != null ? ref : {
+    'https:': 'http:'
+  }) + '//static.duoshuo.com/embed.js';
+  ds.charset = 'UTF-8';
+  return (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ds);
+};
+
+duoshuoQueryRun();
